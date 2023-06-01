2846fb7f103436c50d3034269892cbd7c80e87ea
Nayla Bechara <nbechara@artech-consulting.com>
2023-06-01 10:27:36 -0300

    cambio botones

--- script.js
+++ script.js
@@ -4,20 +4,30 @@ if ('serviceWorker' in navigator) {
     .catch(err => console.warn('Error al tratar de registrar el sw', err))
 }
 
+const displayNotification = document.getElementById('displayNotification')
 const notify = document.getElementById('notify')
-
+let permiso;
 notify.addEventListener("click", () => {
     Notification.requestPermission().then(permission => {
-        if (permission === 'granted') {
-            const notificacion = new Notification('Hola ', {
-                icon: 'https://st4.depositphotos.com/2934765/28878/v/450/depositphotos_288788394-stock-illustration-emoji-smile-icon-vector-symbol.jpg',
-                body: 'Esta es la noitficacion'
-            });
-            notificacion.onclick = function(){
-                window.open('http://google.com');
-            }
-        } else {
-            console.log("no aceptada")
-        }
+        permiso = permission;
+       
     })
-  })
+})
+
+
+displayNotification.addEventListener("click", () => {
+    if (permiso === 'granted') {
+        const notificacion = new Notification('Hola ', {
+            icon: 'https://st4.depositphotos.com/2934765/28878/v/450/depositphotos_288788394-stock-illustration-emoji-smile-icon-vector-symbol.jpg',
+            body: 'Esta es la notificacion'
+        });
+        notificacion.onclick = function(){
+            window.open('http://google.com');
+        }
+    } else {
+        alert("no aceptada")
+    }
+})
+
+
+
