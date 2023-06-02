0d247f9510f025dbc8af4d4bf6f9127509395335
Nayla Bechara <nbechara@artech-consulting.com>
2023-06-02 11:09:20 -0300

    unifico botones

--- script.js
+++ script.js
@@ -4,30 +4,25 @@ if ('serviceWorker' in navigator) {
     .catch(err => console.warn('Error al tratar de registrar el sw', err))
 }
 
-const displayNotification = document.getElementById('displayNotification')
 const notify = document.getElementById('notify')
-let permiso;
+
 notify.addEventListener("click", () => {
     Notification.requestPermission().then(permission => {
-        permiso = permission;
-       
+      
+        if (permission === 'granted') {
+            const notificacion = new Notification('Hola ', {
+                icon: 'https://st4.depositphotos.com/2934765/28878/v/450/depositphotos_288788394-stock-illustration-emoji-smile-icon-vector-symbol.jpg',
+                body: 'Esta es la notificacion'
+            });
+            notificacion.onclick = function(){
+                window.open('http://google.com');
+            }
+        } else {
+            alert("no aceptada")
+        }
     })
 })
 
-
-displayNotification.addEventListener("click", () => {
-    if (permiso === 'granted') {
-        const notificacion = new Notification('Hola ', {
-            icon: 'https://st4.depositphotos.com/2934765/28878/v/450/depositphotos_288788394-stock-illustration-emoji-smile-icon-vector-symbol.jpg',
-            body: 'Esta es la notificacion'
-        });
-        notificacion.onclick = function(){
-            window.open('http://google.com');
-        }
-    } else {
-        alert("no aceptada")
-    }
-})
 
 
 
