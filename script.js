eca612ec800e219aa34068238a8105c0c8dfdb5c
Nayla Bechara <nbechara@artech-consulting.com>
2023-06-02 11:21:41 -0300

    prueba2

--- script.js
+++ script.js
@@ -7,21 +7,37 @@ if ('serviceWorker' in navigator) {
 const notify = document.getElementById('notify')
 
 notify.addEventListener("click", () => {
-    Notification.requestPermission().then(permission => {
-      
-        if (permission === 'granted') {
-            const notificacion = new Notification('Hola ', {
-                icon: 'https://st4.depositphotos.com/2934765/28878/v/450/depositphotos_288788394-stock-illustration-emoji-smile-icon-vector-symbol.jpg',
-                body: 'Esta es la notificacion'
-            });
-            notificacion.onclick = function(){
-                window.open('http://google.com');
-            }
-        } else {
-            alert("no aceptada")
-        }
-    })
+  Notification.requestPermission().then(permission => {
+    if (permission === 'granted') {
+      navigator.serviceWorker.getRegistration().then(registration => {
+        registration.showNotification('Hola', {
+          icon: 'https://st4.depositphotos.com/2934765/28878/v/450/depositphotos_288788394-stock-illustration-emoji-smile-icon-vector-symbol.jpg',
+          body: 'Esta es la notificacion'
+        });
+      });
+    } else {
+      alert("no aceptada")
+    }
+  })
 })
+
+
+// notify.addEventListener("click", () => {
+//     Notification.requestPermission().then(permission => {
+      
+//         if (permission === 'granted') {
+//             const notificacion = new Notification('Hola ', {
+//                 icon: 'https://st4.depositphotos.com/2934765/28878/v/450/depositphotos_288788394-stock-illustration-emoji-smile-icon-vector-symbol.jpg',
+//                 body: 'Esta es la notificacion'
+//             });
+//             notificacion.onclick = function(){
+//                 window.open('http://google.com');
+//             }
+//         } else {
+//             alert("no aceptada")
+//         }
+//     })
+// })
 
 
 
